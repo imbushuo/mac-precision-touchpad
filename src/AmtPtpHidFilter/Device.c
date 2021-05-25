0e3f9897a68743f1abde33178df7fb833f00249e
Bingxing Wang <i@imbushuo.net>
2021-05-25 02:24:48 -0700

    Initial IO queue stuff

--- src/AmtPtpHidFilter/Device.c
+++ src/AmtPtpHidFilter/Device.c
@@ -12,6 +12,85 @@ PtpFilterCreateDevice(
     _Inout_ PWDFDEVICE_INIT DeviceInit
 )
 {
-    UNREFERENCED_PARAMETER(DeviceInit);
+    WDF_OBJECT_ATTRIBUTES deviceAttributes;
+    WDF_PNPPOWER_EVENT_CALLBACKS pnpPowerCallbacks;
+    WDFDEVICE device;
+    PDEVICE_CONTEXT deviceContext;
+    NTSTATUS status;
+
+    PAGED_CODE();
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+
+    // Initialize Power Callback
+    WDF_PNPPOWER_EVENT_CALLBACKS_INIT(&pnpPowerCallbacks);
+    pnpPowerCallbacks.EvtDevicePrepareHardware = PtpFilterPrepareHardware;
+    pnpPowerCallbacks.EvtDeviceD0Entry = PtpFilterDeviceD0Entry;
+    pnpPowerCallbacks.EvtDeviceD0Exit = PtpFilterDeviceD0Exit;
+    WdfDeviceInitSetPnpPowerEventCallbacks(DeviceInit, &pnpPowerCallbacks);
+
+    // Create WDF device object
+    WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&deviceAttributes, DEVICE_CONTEXT);
+    status = WdfDeviceCreate(&DeviceInit, &deviceAttributes, &device);
+    if (!NT_SUCCESS(status)) {
+        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "WdfDeviceCreate failed: %!STATUS!", status);
+        goto exit;
+    }
+
+    // Initialize context and interface
+    deviceContext = PtpFilterGetContext(device);
+    deviceContext->Device = device;
+    deviceContext->DeviceIoTarget = WdfDeviceGetIoTarget(device);
+    status = WdfDeviceCreateDeviceInterface(device,&GUID_DEVICEINTERFACE_AmtPtpHidFilter, NULL);
+    if (!NT_SUCCESS(status)) {
+        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "WdfDeviceCreateDeviceInterface failed: %!STATUS!", status);
+        goto exit;
+    }
+
+    // Initialize IO queue
+    status = PtpFilterIoQueueInitialize(device);
+    if (!NT_SUCCESS(status)) {
+        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "PtpFilterIoQueueInitialize failed: %!STATUS!", status);
+    }
+
+exit:
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit, Status = %!STATUS!", status);
+    return status;
+}
+
+NTSTATUS
+PtpFilterPrepareHardware(
+    _In_ WDFDEVICE Device,
+    _In_ WDFCMRESLIST ResourceList,
+    _In_ WDFCMRESLIST ResourceListTranslated
+)
+{
+    UNREFERENCED_PARAMETER(Device);
+    UNREFERENCED_PARAMETER(ResourceList);
+    UNREFERENCED_PARAMETER(ResourceListTranslated);
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+PtpFilterDeviceD0Entry(
+    _In_ WDFDEVICE Device,
+    _In_ WDF_POWER_DEVICE_STATE PreviousState
+)
+{
+    UNREFERENCED_PARAMETER(Device);
+    UNREFERENCED_PARAMETER(PreviousState);
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+PtpFilterDeviceD0Exit(
+    _In_ WDFDEVICE Device,
+    _In_ WDF_POWER_DEVICE_STATE TargetState
+)
+{
+    UNREFERENCED_PARAMETER(Device);
+    UNREFERENCED_PARAMETER(TargetState);
+
     return STATUS_SUCCESS;
 }
---
+++ src/AmtPtpHidFilter/include/Queue.h
@@ -0,0 +1,7 @@
+// Queue.h: IO queue bits
+#pragma once
+
+NTSTATUS
+PtpFilterIoQueueInitialize(
+    _In_ WDFDEVICE Device
+);
