4b831562bc0239d06165009ace0564d150f6efd0
Bingxing Wang <i@imbushuo.net>
2021-05-29 17:16:45 -0700

    Input: some attempt to kick it into multi-touch mode. Need to check report reads

--- src/AmtPtpHidFilter/include/Device.h
+++ src/AmtPtpHidFilter/include/Device.h
@@ -8,6 +8,14 @@ EXTERN_C_START
 DEFINE_GUID(GUID_DEVICEINTERFACE_AmtPtpHidFilter,
     0xff969022, 0x3111, 0x4441, 0x8f, 0x88, 0x87, 0x54, 0x40, 0x17, 0x2c, 0x2e);
 
+// Definition
+#define HID_XFER_PACKET_SIZE 32
+
+#define HID_VID_APPLE_USB 0x05ac
+#define HID_VID_APPLE_BT  0x004c
+
+#define HID_PID_MAGIC_TRACKPAD_2 0x0265
+
 // Device Context
 typedef struct _DEVICE_CONTEXT
 {
@@ -22,19 +30,15 @@ typedef struct _DEVICE_CONTEXT
 
     // System HID transport PDO IO Target
     WDFIOTARGET HidIoTarget;
+    BOOLEAN     IsHidIoDetourCompleted;
 } DEVICE_CONTEXT, *PDEVICE_CONTEXT;
 
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(DEVICE_CONTEXT, PtpFilterGetContext)
 
-// Initialization routine
+// Initialization routines
 NTSTATUS
 PtpFilterCreateDevice(
     _Inout_ PWDFDEVICE_INIT DeviceInit
-);
-
-NTSTATUS
-PtpFilterHijackWindowsHIDStack(
-    _In_ WDFDEVICE Device
 );
 
 // Power Management Handlers
@@ -42,5 +46,11 @@ EVT_WDF_DEVICE_PREPARE_HARDWARE PtpFilterPrepareHardware;
 EVT_WDF_DEVICE_D0_ENTRY PtpFilterDeviceD0Entry;
 EVT_WDF_DEVICE_D0_EXIT PtpFilterDeviceD0Exit;
 EVT_WDF_DEVICE_SELF_MANAGED_IO_INIT PtpFilterSelfManagedIoInit;
+
+// Device Management routines
+NTSTATUS
+PtpFilterConfigureMultiTouch(
+    _In_ WDFDEVICE Device
+);
 
 EXTERN_C_END
