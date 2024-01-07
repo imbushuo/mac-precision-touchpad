7e3d1d27ef963568f4b0be4c51e12d6ed5fae6d8
Avery Black <avery.c.black@gmail.com>
2024-01-07 12:50:35 -0800

    Use report id to verify input reports

--- src/AmtPtpHidFilter/Input.c
+++ src/AmtPtpHidFilter/Input.c
@@ -116,67 +116,45 @@ PtpFilterInputIssueTransportRequest(
 	}
 }
 
+static
 VOID
-PtpFilterInputRequestCompletionCallback(
-	_In_ WDFREQUEST Request,
-	_In_ WDFIOTARGET Target,
-	_In_ PWDF_REQUEST_COMPLETION_PARAMS Params,
-	_In_ WDFCONTEXT Context
+PtpFilterInputParseMT2Report(
+	_In_ PUCHAR Buffer,
+	_In_ SIZE_T BufferLength,
+	_In_ PDEVICE_CONTEXT DeviceContext
 )
 {
-	PWORKER_REQUEST_CONTEXT requestContext;
-	PDEVICE_CONTEXT deviceContext;
 	NTSTATUS status;
 
 	WDFREQUEST ptpRequest;
-	PTP_REPORT ptpOutputReport;
 	WDFMEMORY  ptpRequestMemory;
+	PTP_REPORT ptpOutputReport;
 
-	size_t responseLength;
-	PUCHAR responseBuffer;
-
-	UINT32 timestamp;
-	size_t raw_n = 0;
-	INT x, y = 0;
-
-	const TRACKPAD_FINGER_TYPE5* f;
 	const TRACKPAD_REPORT_TYPE5* mt_report;
+	const TRACKPAD_FINGER_TYPE5* f;
+	SIZE_T raw_n;
+	INT x, y = 0;
+	UINT32 timestamp;
 
-	UNREFERENCED_PARAMETER(Target);
-	
-	requestContext = (PWORKER_REQUEST_CONTEXT)Context;
-	deviceContext = requestContext->DeviceContext;
-	responseLength = (size_t)(LONG)WdfRequestGetInformation(Request);
-	responseBuffer = WdfMemoryGetBuffer(Params->Parameters.Ioctl.Output.Buffer, NULL);
+	mt_report = (const TRACKPAD_REPORT_TYPE5*)Buffer;
 
-	// Pre-flight check 0: Right now we only have Magic Trackpad 2 (BT and USB)
-	if (deviceContext->VendorID != HID_VID_APPLE_USB && deviceContext->VendorID != HID_VID_APPLE_BT) {
-		TraceEvents(TRACE_LEVEL_ERROR, TRACE_INPUT, "%!FUNC! Unsupported device entered this routine");
-		WdfDeviceSetFailed(deviceContext->Device, WdfDeviceFailedNoRestart);
-		goto cleanup;
+	// Pre-flight check 1: the response size should be sane
+	if (BufferLength < sizeof(TRACKPAD_REPORT_TYPE5) ||
+		(BufferLength - sizeof(TRACKPAD_REPORT_TYPE5)) % sizeof(TRACKPAD_FINGER_TYPE5) != 0)
+	{
+		TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_INPUT, "%!FUNC! Malformed input received. Length = %llu. Attempt to reconfigure the device.", BufferLength);
+		WdfTimerStart(DeviceContext->HidTransportRecoveryTimer, WDF_REL_TIMEOUT_IN_SEC(3));
+		return;
 	}
 
-	// Pre-flight check 1: if size is 0, this is not something we need. Ignore the read, and issue next request.
-	if (responseLength <= 0) {
-		WdfWorkItemEnqueue(requestContext->DeviceContext->HidTransportRecoveryWorkItem);
-		goto cleanup;
-	}
-
-	// Pre-flight check 2: the response size should be sane
-	if (responseLength < sizeof(TRACKPAD_REPORT_TYPE5) || (responseLength - sizeof(TRACKPAD_REPORT_TYPE5)) % sizeof(TRACKPAD_FINGER_TYPE5) != 0) {
-		TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_INPUT, "%!FUNC! Malformed input received. Length = %llu. Attempt to reconfigure the device.", responseLength);
-		WdfTimerStart(deviceContext->HidTransportRecoveryTimer, WDF_REL_TIMEOUT_IN_SEC(3));
-		goto cleanup;
-	}
-
-	// Read report and fulfill PTP request. If no report is found, just exit.
-	status = WdfIoQueueRetrieveNextRequest(deviceContext->HidReadQueue, &ptpRequest);
-	if (!NT_SUCCESS(status)) {
+	// Retrieve PTP output 
+	status = WdfIoQueueRetrieveNextRequest(DeviceContext->HidReadQueue, &ptpRequest);
+	if (!NT_SUCCESS(status))
+	{
 		TraceEvents(TRACE_LEVEL_ERROR, TRACE_INPUT, "%!FUNC! WdfIoQueueRetrieveNextRequest failed with %!STATUS!", status);
-		goto cleanup;
+		return;
 	}
 
-	mt_report = (TRACKPAD_REPORT_TYPE5*) responseBuffer;
 	timestamp = (mt_report->TimestampHigh << 5) | mt_report->TimestampLow;
 
 	// Report header
@@ -186,7 +164,7 @@ PtpFilterInputRequestCompletionCallback(
 
 	// Report required content
 	// Touch
-	raw_n = (responseLength - sizeof(TRACKPAD_REPORT_TYPE5)) / sizeof(TRACKPAD_FINGER_TYPE5);
+	raw_n = (BufferLength - sizeof(TRACKPAD_REPORT_TYPE5)) / sizeof(TRACKPAD_FINGER_TYPE5);
 	if (raw_n >= PTP_MAX_CONTACT_POINTS) raw_n = PTP_MAX_CONTACT_POINTS;
 	ptpOutputReport.ContactCount = (UCHAR) raw_n;
 	for (size_t i = 0; i < raw_n; i++) {
@@ -195,13 +173,13 @@ PtpFilterInputRequestCompletionCallback(
 		// Sign extend
 		x = (SHORT)(f->AbsoluteX << 3) >> 3;
 		y = -(SHORT)(f->AbsoluteY << 3) >> 3;
-		x = (x - deviceContext->X.min) > 0 ? (x - deviceContext->X.min) : 0;
-		y = (y - deviceContext->Y.min) > 0 ? (y - deviceContext->Y.min) : 0;
+		x = (x - DeviceContext->X.min) > 0 ? (x - DeviceContext->X.min) : 0;
+		y = (y - DeviceContext->Y.min) > 0 ? (y - DeviceContext->Y.min) : 0;
 
 		ptpOutputReport.Contacts[i].ContactID = f->Id;
 		ptpOutputReport.Contacts[i].X = (USHORT)x;
 		ptpOutputReport.Contacts[i].Y = (USHORT)y;
-		
+
 		// 0x1 = Transition between states
 		// 0x2 = Floating finger
 		// 0x4 = Contact/Valid
@@ -224,20 +202,99 @@ PtpFilterInputRequestCompletionCallback(
 	if (!NT_SUCCESS(status))
 	{
 		TraceEvents(TRACE_LEVEL_ERROR, TRACE_INPUT, "%!FUNC! WdfRequestRetrieveOutputBuffer failed with %!STATUS!", status);
-		WdfDeviceSetFailed(deviceContext->Device, WdfDeviceFailedAttemptRestart);
-		goto cleanup;
+		return;
 	}
 
-	status = WdfMemoryCopyFromBuffer(ptpRequestMemory, 0, (PVOID) &ptpOutputReport, sizeof(PTP_REPORT));
+	status = WdfMemoryCopyFromBuffer(ptpRequestMemory, 0, (PVOID)&ptpOutputReport, sizeof(PTP_REPORT));
 	if (!NT_SUCCESS(status))
 	{
 		TraceEvents(TRACE_LEVEL_ERROR, TRACE_INPUT, "%!FUNC! WdfMemoryCopyFromBuffer failed with %!STATUS!", status);
-		WdfDeviceSetFailed(deviceContext->Device, WdfDeviceFailedAttemptRestart);
-		goto cleanup;
+		WdfDeviceSetFailed(DeviceContext->Device, WdfDeviceFailedAttemptRestart);
+		return;
 	}
 
 	WdfRequestSetInformation(ptpRequest, sizeof(PTP_REPORT));
 	WdfRequestComplete(ptpRequest, status);
+}
+
+static
+VOID
+PtpFilterInputParseReport(
+	_In_ PUCHAR Buffer,
+	_In_ SIZE_T BufferLength,
+	_In_ PDEVICE_CONTEXT DeviceContext
+)
+{
+	UCHAR reportId = Buffer[0];
+
+	switch (reportId) {
+	case 0x2: // Mouse report
+		// USB devices have mouse reports prepended, so skip over it to get to next input report
+		if (BufferLength > sizeof(MOUSE_REPORT))
+		{
+			Buffer += sizeof(MOUSE_REPORT);
+			BufferLength -= sizeof(MOUSE_REPORT);
+			PtpFilterInputParseReport(Buffer, BufferLength, DeviceContext);
+		}
+		else
+		{
+			TraceEvents(TRACE_LEVEL_WARNING, TRACE_INPUT,
+				"%!FUNC! Mouse Packet - Setting Wellspring mode");
+			WdfTimerStart(DeviceContext->HidTransportRecoveryTimer, WDF_REL_TIMEOUT_IN_SEC(3));
+		}
+		break;
+
+	case 0x31: // MT2 report
+		PtpFilterInputParseMT2Report(Buffer, BufferLength, DeviceContext);
+		break;
+
+	case 0xF7: // Two packets in one (0xF7, pkt 1 len, <pkt 1>, <pkt 2>)
+	case 0xFC: // Part one of large packet
+	case 0xFE: // Part two of large packet
+	case 0x90: // Battery status
+	default:
+		TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_INPUT,
+			"%!FUNC! Unhandled packet (Report ID: 0x%x)", reportId);
+		WdfWorkItemEnqueue(DeviceContext->HidTransportRecoveryWorkItem);
+		break;
+	}
+}
+
+VOID
+PtpFilterInputRequestCompletionCallback(
+	_In_ WDFREQUEST Request,
+	_In_ WDFIOTARGET Target,
+	_In_ PWDF_REQUEST_COMPLETION_PARAMS Params,
+	_In_ WDFCONTEXT Context
+)
+{
+	PWORKER_REQUEST_CONTEXT requestContext;
+	PDEVICE_CONTEXT deviceContext;
+
+	size_t responseLength;
+	PUCHAR responseBuffer;
+
+	UNREFERENCED_PARAMETER(Target);
+	
+	requestContext = (PWORKER_REQUEST_CONTEXT)Context;
+	deviceContext = requestContext->DeviceContext;
+	responseLength = (size_t)(LONG)WdfRequestGetInformation(Request);
+	responseBuffer = WdfMemoryGetBuffer(Params->Parameters.Ioctl.Output.Buffer, NULL);
+
+	// Pre-flight check 0: Right now we only have Magic Trackpad 2 (BT and USB)
+	if (deviceContext->VendorID != HID_VID_APPLE_USB && deviceContext->VendorID != HID_VID_APPLE_BT) {
+		TraceEvents(TRACE_LEVEL_ERROR, TRACE_INPUT, "%!FUNC! Unsupported device entered this routine");
+		WdfDeviceSetFailed(deviceContext->Device, WdfDeviceFailedNoRestart);
+		goto cleanup;
+	}
+
+	// Pre-flight check 1: if size is 0, this is not something we need. Ignore the read, and issue next request.
+	if (responseLength <= 0) {
+		WdfWorkItemEnqueue(requestContext->DeviceContext->HidTransportRecoveryWorkItem);
+		goto cleanup;
+	}
+
+	PtpFilterInputParseReport(responseBuffer, responseLength, deviceContext);
 
 cleanup:
 	// Cleanup
